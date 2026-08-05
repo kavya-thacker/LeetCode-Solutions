f4d9ad1d090058d91e8e320abd0f5867714e3057
kavya-thacker <kavya.h.thacker@gmail.com>
2026-08-05 20:35:02 +0530

    Time: 3 ms (18.42%) | Memory: 47.9 MB (38.79%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+     int i =0;
+     int j =1;
+
+     if(nums.length==0 || nums.length==1) return;
+     while(i<nums.length&&j<nums.length){
+        if(nums[i]==0 && nums[j]!=0) {
+            nums[i]=nums[j];
+            nums[j]= 0;
+           i++;j++;
+        }
+        else if(nums[i]==0 && nums[j]==0) j++;
+        else{i++;j++;}
+     }
+    }
+}
